0ef90d2b69c0ab32f644dac77b645f6fe8f0d1a7
Tomoya Okuyama <nitunamoni13579@gmail.com>
2023-11-03 15:08:04 +0900

    feat: add test one

--- get_next_line/tests/srcs/bonus/_get_next_line_test/_gnl_one.c
+++ get_next_line/tests/srcs/bonus/_get_next_line_test/_gnl_one.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   _gnl_multiline_endline.c                           :+:      :+:    :+:   */
+/*   _gnl_one.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tookuyam <tookuyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 13:31:30 by tookuyam          #+#    #+#             */
-/*   Updated: 2023/11/03 15:07:20 by tookuyam         ###   ########.fr       */
+/*   Created: 2023/11/03 15:04:55 by tookuyam          #+#    #+#             */
+/*   Updated: 2023/11/03 15:07:40 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define FILE_NAME "files/gnl/multiline_endline.txt"
+#define FILE_NAME "files/gnl/one.txt"
 
-TEST(_get_next_line, multiline_endline)
+TEST(_get_next_line, one)
 {
 	int		fd;
 
 	fd = open(FILE_NAME, O_RDONLY);
 	_gnl_test(-1, NULL); // 初期化
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
-	ASSERT_EQ(_gnl_test(fd, "0123456789\n"), 0);
+	ASSERT_EQ(_gnl_test(fd, "a"), 0);
 	ASSERT_EQ(_gnl_test(fd, NULL), 0);
 	ASSERT_TRUE(leak_check() == 0);
 	_gnl_test(-1, NULL); // 初期化
