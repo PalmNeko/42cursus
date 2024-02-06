44aab94595ac40836020bdcc1fd8f1ae7cc37912
tookuyam <tookuyam@student.42.tokyo.jp>
2024-02-06 12:26:57 +0900

    ft_printf: add: test functions

--- ft_printf/tests/ft_printf_test.c
+++ ft_printf/tests/ft_printf_test.c
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_test.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:26:23 by tookuyam          #+#    #+#             */
-/*   Updated: 2023/11/09 15:17:40 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/02/06 12:25:32 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tyctest.h"
 #include "ft_printf.h"
+#include "ft_printf_test_tool.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <limits.h>
 
 TEST(ft_printf, francinette)
 {
 	*failure_flag = 0;
-	printf("%d\n", ft_printf("\001\002\007\v\010\f\r\n"));
+	ASSERT_TRUE(check_ft_printf("\001\002\007\v\010\f\r\n"));
 }
